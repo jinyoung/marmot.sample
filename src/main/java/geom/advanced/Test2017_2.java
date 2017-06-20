1595af05c29b3a0dfb26def72c905c1390988807
kwlee <kwlee@sbdata99.gsbd.etri.re.kr>
2017-06-20 15:14:22 +0900

    .

---
+++ src/main/java/geom/advanced/Test2017_2.java
@@ -0,0 +1,52 @@
+package geom.advanced;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import com.vividsolutions.jts.geom.Envelope;
+
+import basic.SampleUtils;
+import marmot.DataSet;
+import marmot.Program;
+import marmot.optor.AggregateFunction;
+import marmot.remote.RemoteMarmotConnector;
+import marmot.remote.robj.MarmotClient;
+import utils.DimensionDouble;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public class Test2017_2 {
+	private static final String ADDR_BLD = "건물/위치";
+	private static final String ADDR_BLD_UTILS = "tmp/test2017/buildings_utils";
+	private static final String GRID = "tmp/test2017/grid30";
+	
+	public static final void main(String... args) throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		
+		// 원격 MarmotServer에 접속.
+		RemoteMarmotConnector connector = new RemoteMarmotConnector();
+		MarmotClient marmot = connector.connect("localhost", 12985);
+
+		DataSet info = marmot.getDataSet(ADDR_BLD);
+		Envelope bounds = info.getBounds();
+		DimensionDouble cellSize = new DimensionDouble(30, 30);
+		
+		Program program = Program.builder("get_biz_grid")
+								.load(ADDR_BLD_UTILS)
+								.buffer("the_geom", "buffer", 100, 16)
+								.assignSquareGridCell("buffer", bounds, cellSize)
+								.centroid("cell_geom", "cell_geom")
+								.intersects("cell_geom", "the_geom")
+								.groupBy("cell_id")
+									.taggedKeyColumns("cell_geom")
+									.aggregate(AggregateFunction.COUNT())
+								.project("cell_geom as the_geom,*-{cell_geom}")
+								.store(GRID)
+								.build();
+		marmot.deleteDataSet(GRID);
+		DataSet result = marmot.createDataSet(GRID, "the_geom", info.getSRID(), program);
+		
+		SampleUtils.printPrefix(result, 5);
+	}
+}
